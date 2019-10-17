eea4f471cb5c8a23921d404811cf547c9a27b567
3939 <yuukiyuuki223@gmail.com>
2019-10-17 15:28:18 +0900

    Ctrl + Alt + K

---
+++ app/src/main/java/com/example/touhoudanmaku/Option_Activity.java
@@ -0,0 +1,23 @@
+package com.example.touhoudanmaku;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Option_Activity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.option_layout);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //全画面表示にする
+        View decor = this.getWindow().getDecorView();
+        decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+    }
+}
--- app/src/main/java/com/example/touhoudanmaku/TitleActivity.java
+++ app/src/main/java/com/example/touhoudanmaku/TitleActivity.java
@@ -2,7 +2,6 @@ package com.example.touhoudanmaku;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 
